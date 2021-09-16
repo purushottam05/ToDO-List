a51c0830a601f2bec1e500ecfcfb4cb126d100b3
purushottam <55759055+puruwastaken@users.noreply.github.com>
2021-09-16 22:08:08 +0530

    Initial commit

---
+++ build/precache-manifest.10bcc9e0e95f645f0acffed4b696c56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5680c9a44716ba18238921ec67cdfc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af2b136c9fe83233675",
+    "url": "/static/css/main.aa1c3f4a.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af2b136c9fe83233675",
+    "url": "/static/js/main.62d0baa1.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
